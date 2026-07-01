1fe432f55228e0924454b364f39913da45710c46
Mizbahuddin Qasim <qasim@techbeeps.co.in>
2026-07-01 15:01:22 +0530

    Update next and react to latest

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
+  distDir: "build",
   reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
-    domains: ['admin.bookwindow.in','bookwindow.in'],
+    // images.domains is deprecated, so you can remove it
     remotePatterns: [
       {
         protocol: "https",
         hostname: "admin.bookwindow.in",
+      },
+      {
+        protocol: "https",
+        hostname: "bookwindow.in",
       },
     ],
   },
